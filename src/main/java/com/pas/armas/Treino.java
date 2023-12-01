7a46aadfd5261b480714dc73e2ad87114ee271af
IthaloCarneiro6 <ithalo.ctn0616@hotmail.com>
2023-12-01 14:51:51 -0300

    firts commit

---
+++ src/main/java/com/pas/armas/Treino.java
@@ -0,0 +1,30 @@
+package com.pas.armas;
+
+public class Treino {
+    private String nome;
+    private String tipo;
+
+    public Treino() {
+    }
+
+    public Treino(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/test/java/com/pas/armas/TreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pas.armas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
