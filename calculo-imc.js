741798bb83848f234bb4c7ccc97634b2ff7c3e94
renanamd <renanalmeida2003@gmail.com>
2023-12-05 21:52:38 -0300

    F

--- calculo-imc.js
+++ calculo-imc.js
@@ -1,7 +1,8 @@
+/*
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
 const app = express();
 const port = 5000; // Escolha a porta desejada
 
@@ -42,3 +43,5 @@ app.get('/calcularimc', async function (req, res) {
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
+*/
